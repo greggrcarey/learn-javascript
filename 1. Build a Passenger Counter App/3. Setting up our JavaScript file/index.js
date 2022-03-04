942d86a938f5d1fbb20f99f138fe08e2a0e01dd4
Gregg Carey <gregg.carey@outlook.com>
2022-03-04 16:58:02 -0600

    17_28

---
+++ 1. Build a Passenger Counter App/3. Setting up our JavaScript file/index.js
@@ -0,0 +1,10 @@
+// document.getElementById("count-el").innerText = 6;
+
+// let count = 5 * 7;
+
+// console.log(count);
+
+let myAge = 43;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
